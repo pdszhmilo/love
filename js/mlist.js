75cfa968e9b34d50e5ea6526fcc01a3e7e64cc4c
pdszhmilo <244091888@qq.com>
2020-02-27 14:20:43 +0800

    sdasd

---
+++ js/mlist.js
@@ -0,0 +1,10 @@
+﻿var wenkmList=[{
+song_album:"",
+song_album1:"",
+song_file:"/",
+
+song_name:"Alan Walker - Faded|Alan Walker - The Spectre|Alan Walker - Alone".split("|"),		//添加网易云音乐id+wy 用 | 隔开
+
+song_id:"36990266wy|506092035wy|444269135wy".split("|") 		//添加网易云音乐id+wy 用 | 隔开
+
+}];
